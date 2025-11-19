527eee75690a3178f29032b302f83ea46d91042f
nicogreco05 <nicologreco074@gmail.com>
2025-11-19 19:39:29 +0100

    Delete src directory

--- src/Rubrica.java
+++
@@ -1,7 +0,0 @@
-public interface Rubrica {
-    String aggiungiContatto(String nome, String num);
-    String aggiornaContatto(String nome, String num);
-    String trovaContatto(String nome);
-    String rimuoviContatto(String nome);
-    String stampaRubrica();
-}
--- src/StoricoAccessi.java
+++
@@ -1,7 +0,0 @@
-public interface StoricoAccessi{
-    String aggiungiEvento(Utente utente);
-    void rimuoviEvento();
-    String cercaEvento(Utente utente);
-    String visualizzaStorico();
-
-}
